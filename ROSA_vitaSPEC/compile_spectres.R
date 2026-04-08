3ab39a18095fa29c3a7b00d6b5a3fa867e5d984f
mat_rossa <matson433@gmail.com>
2026-04-08 15:23:01 +0200

    HR ready

--- ROSA_vitaSPEC/compile_spectres.R
+++ ROSA_vitaSPEC/compile_spectres.R
@@ -1,15 +1,16 @@
-compile_spectres_matrix <- function(data, colonnes_spectres, filtre_nom = "aucun") {
+compile_spectres_matrix <- function(..., colonnes_spectres, filtre_nom = "aucun") {
+  
+  data <- bind_rows(list(...))
   
   table <- list()
   
   for (i in colonnes_spectres) {
-    
     df_temp <- data
     df_temp$x <- df_temp[[i]]
     df_temp <- df_temp[!is.na(df_temp$x[, 1]), ]
     df_temp <- df_temp %>% select(-starts_with("x20"))
     table[[i]] <- df_temp
-  }
+    }
   df_compile <- bind_rows(table)
   
   if (filtre_nom == "sans_X") {
@@ -21,20 +22,19 @@ compile_spectres_matrix <- function(data, colonnes_spectres, filtre_nom = "aucun
   return(df_compile)
 }
 
-
-
-compile_spectres_df <- function(data, colonnes_spectres, filtre_nom = "aucun") {
+compile_spectres_df <- function(..., colonnes_spectres, filtre_nom = "aucun") {
+  
+  data <- bind_rows(list(...))
   
   table <- list()
   
   for (i in colonnes_spectres) {
-    
     df_temp <- data
     df_temp$x <- df_temp[[i]]
     df_temp <- df_temp[!is.na(df_temp$x[, 1]), ]
     df_temp <- df_temp %>% select(-starts_with("x20"))
     table[[i]] <- df_temp
-  }
+    }
   df_compile <- bind_rows(table)
   
   if (filtre_nom == "sans_X") {
@@ -50,25 +50,3 @@ compile_spectres_df <- function(data, colonnes_spectres, filtre_nom = "aucun") {
   
   return(df_compile)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
