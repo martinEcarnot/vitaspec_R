a85c38998ab6a24a16d8aa4125253f71349f8a07
Martin Ecarnot <ecarnotm@io-login-03.io.internal>
2026-06-16 09:59:22 +0200

    recovery

--- ROSA_vitaSPEC/TEST_cluster.R
+++
@@ -1,4 +0,0 @@
-read.table(iris)
-plot(iris$Sepal.Length, iris$Sepal.Width)
-
-ggsave("plot_test.pdf")
--- TEST_cluster.R
+++
@@ -1,4 +0,0 @@
-read.table(iris)
-plot(iris$Sepal.Length, iris$Sepal.Width)
-
-ggsave("/replicated/data/plot_test.pdf")
